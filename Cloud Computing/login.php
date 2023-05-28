aaca78c305bb4eed05ac6309f24f93644f9a630f
Yazid Ramdhani <125806024+yazidramdhani@users.noreply.github.com>
2023-05-29 00:02:05 +0700

    Update login.php
    add email

--- Cloud Computing/login.php
+++ Cloud Computing/login.php
@@ -2,7 +2,7 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
+    $emailOrUsername = $_POST["emailOrUsername"];
     $password = $_POST["password"];
 
     $conn = mysqli_connect("localhost", "root", "", "agroaid_db");
@@ -10,14 +10,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $sql = "SELECT * FROM users WHERE (email = '$emailOrUsername' OR username = '$emailOrUsername') AND password = '$password'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) == 1) {
-        $_SESSION["username"] = $username;
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION["username"] = $row["username"];
         header("Location: dashboard.php");
     } else {
-        echo "Invalid username or password";
+        echo "Invalid email/username or password";
     }
 
     mysqli_close($conn);
@@ -32,8 +33,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <h2>Login</h2>
     <form method="POST" action="login.php">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
+        <label for="emailOrUsername">Email or Username:</label>
+        <input type="text" name="emailOrUsername" required><br>
 
         <label for="password">Password:</label>
         <input type="password" name="password" required><br>
