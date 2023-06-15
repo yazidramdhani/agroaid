bbf890f2673d944931304751d050bc19db3c460e
Yazid Ramdhani <125806024+yazidramdhani@users.noreply.github.com>
2023-06-15 15:41:04 +0700

    add like and unlike reply

--- Cloud Computing/src/handler/reply_handler.js
+++ Cloud Computing/src/handler/reply_handler.js
@@ -35,7 +35,59 @@ const getAllRepliesByComment = async (request, h) => {
     return response;
 };
 
+const likeReply = async (request, h) => {
+  const { replyId } = request.params;
+  const reply = await Reply.findByPk(replyId);
+
+  if (!reply) {
+    return h
+      .response({
+        status: "error",
+        message: "Reply not found",
+      })
+      .code(404);
+  }
+
+  reply.likes += 1;
+  await reply.save();
+
+  return h
+    .response({
+      status: "success",
+      message: "Reply liked successfully",
+      data: reply,
+    })
+    .code(200);
+};
+
+const unlikeReply = async (request, h) => {
+  const { replyId } = request.params;
+  const reply = await Reply.findByPk(replyId);
+
+  if (!reply) {
+    return h
+      .response({
+        status: "error",
+        message: "Reply not found",
+      })
+      .code(404);
+  }
+
+  reply.likes -= 1;
+  await reply.save();
+
+  return h
+    .response({
+      status: "success",
+      message: "Reply unliked successfully",
+      data: reply,
+    })
+    .code(200);
+};
+
 module.exports = {
-    addReply,
-    getAllRepliesByComment
+  addReply,
+  getAllRepliesByComment,
+  likeReply,
+  unlikeReply,
 };
